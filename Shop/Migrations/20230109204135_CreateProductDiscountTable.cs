c9b8f78b8c26312fd5aab7406ab0e0943961ab61
UkarainskiyS <https://github.com/UkrainskiyS>
2023-01-09 23:50:24 +0300

    add ProductDiscount migration

---
+++ Shop/Migrations/20230109204135_CreateProductDiscountTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateProductDiscountTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductDiscount",
+                columns: table => new
+                {
+                    ProductId = table.Column<long>(type: "bigint", nullable: false),
+                    Discount = table.Column<int>(type: "smallint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.ForeignKey(
+                        name: "FK_ProductDiscount_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "ProductDiscount");
+        }
+    }
+}
